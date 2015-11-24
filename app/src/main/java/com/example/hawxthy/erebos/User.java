a3f6088dcbe3b4cbd1ce02897ecdde6b1fcbb88b
t_rust01 <tristan.rust@gmx.de>
2015-11-24 16:27:53 +0100

    First

---
+++ app/src/main/java/com/example/hawxthy/erebos/User.java
@@ -0,0 +1,16 @@
+package com.example.hawxthy.erebos;
+
+/**
+ * Created by HAWXTHY on 24.11.2015.
+ */
+public class User {
+    private String mName;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String value) {
+        mName = value;
+    }
+}
