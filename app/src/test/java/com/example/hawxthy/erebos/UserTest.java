b967970ee1b5bfb6838637ed6e27b1c7f048334a
t_rust01 <tristan.rust@gmx.de>
2015-11-24 16:28:13 +0100

    First

---
+++ app/src/test/java/com/example/hawxthy/erebos/UserTest.java
@@ -0,0 +1,23 @@
+package com.example.hawxthy.erebos;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21, manifest = "src/main/AndroidManifest.xml")
+public class UserTest {
+
+    @Test
+    public void UserNameTest() {
+        User u = new User();
+
+        u.setName("foo");
+
+        Assert.assertEquals("foo", u.getName());
+    }
+
+}
