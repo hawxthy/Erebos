86036506de2fd259ca8f160e2f7942a92e79165c
t_rust01 <tristan.rust@gmx.de>
2015-12-08 19:34:48 +0100

    14th

---
+++ app/src/androidTest/java/com/example/hawxthy/erebos/ChangeTextBehaviorTest.java
@@ -0,0 +1,53 @@
+package com.example.hawxthy.erebos;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.Until;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
+public class ChangeTextBehaviorTest {
+
+    private static final String BASIC_SAMPLE_PACKAGE = "com.example.android.testing.uiautomator.BasicSample";
+    private static final int LAUNCH_TIMEOUT = 5000;
+    private static final String STRING_TO_BE_TYPED = "UiAutomator";
+    private UiDevice mDevice;
+
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        // Initialize UiDevice instance
+
+    }
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+    /*
+    public void setUp() {
+        // Launch a simple calculator app
+        Context context = getInstrumentation().getContext();
+        Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(CALC_PACKAGE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        // Clear out any previous instances
+        context.startActivity(intent);
+        mDevice.wait(Until.hasObject(By.pkg(CALC_PACKAGE).depth(0)), TIMEOUT);
+    }
+    */
+}
